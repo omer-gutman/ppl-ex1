82649577f50029e57f9099089b26f34b4be81001
omer-gutman <guom2003@gmail.com>
2026-04-28 19:16:15 +0300

    completed Part 3: A Gentle Introduction to Monads and Part 2: countVowels and treeToSetence functions

--- ex1_template/src/part2.ts
+++ ex1_template/src/part2.ts
@@ -4,7 +4,12 @@ const stringToArray = R.split("");
 
 /* Question 2.1 */
 const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-export const countVowels: (s: string) => number = undefined as any;;
+
+export const countVowels: (s: string) => number = (S) => {
+    const characters = stringToArray(S);
+    const onlyVowesls = characters.filter(char => vowels.includes(char.toLocaleLowerCase()));
+    return onlyVowesls.length;
+}
 
 /* Question 2.2 */
 export const isPalindrome = (text: string): boolean => undefined as any;;
@@ -16,4 +21,8 @@ export type WordTree = {
     children: WordTree[];
 }
 
-export const treeToSentence = (t: WordTree): string => undefined as any;;
+export const treeToSentence = (t: WordTree): string =>  {
+    return t.children.reduce((acc, child) =>  {
+        return acc + " " + treeToSentence(child);
+    }, t.root);
+}
--- ex1_template/src/part3.ts
+++ ex1_template/src/part3.ts
@@ -8,7 +8,10 @@ const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
     throw "No element found.";
 }
 
-export const findResult = <T>(pred: (x: T) => boolean, a: T[]): Result<T> => undefined as any;
+export const findResult = <T>(pred: (x: T) => boolean, a: T[]): Result<T> => {
+    const item = a.find(pred);
+    return item === undefined ? makeFailure("No element found.") : makeOk(item);
+}
 
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
@@ -20,6 +23,17 @@ const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
     }
 }
 
-export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> => undefined as any;
-export const returnSquaredIfFoundEven_v3 = (a: number[]): number => undefined as any;
+export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> => {
+    return bind(
+        findResult(x => x % 2 === 0, a),
+        x => makeOk(x * x)
+    )
+}
+export const returnSquaredIfFoundEven_v3 = (a: number[]): number => {
+    return either(
+        findResult(x => x % 2 === 0, a),
+        x => x * x, 
+        (err) => -1
+    )
+}
 
